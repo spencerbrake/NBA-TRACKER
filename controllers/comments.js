44a9e39405ae4d479f9c0e6d9f036aa8297b8076
spencer brake <spencerbrake98@gmail.com>
2021-06-23 18:16:19 -0700

    comment functionality

--- controllers/comments.js
+++ controllers/comments.js
@@ -5,12 +5,17 @@ module.exports = {
 };
 
 function create(req, res) {
-    Player.findById(req.paramas.id, function(err, player){
-        req.body.userId = req.user._id;
-        req.body.username = req.user.name;
+    console.log(req.params.id);
+    Player.findById(req.params.id, function(err, player){
+        req.body.user = req.user._id;
+        req.body.userName = req.user.name;
+        console.log(req.body.user, 'req.body.user');
+        console.log(req.body.userName, 'req.body.userName');
+        console.log(player.comments, 'player.comments before');
         player.comments.push(req.body);
+        console.log(player.comments, 'player.comments after');
         player.save(function(err){
-            res.redirect(`/movies/${player._id}`);
+            res.redirect(`/players/${player._id}`);
         });
     });
 }
--- routes/comments.js
+++ routes/comments.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const commentsCtrl = require('../controllers/comments');
 
-router.post('/movies/:id/comments', commentsCtrl.create);
+router.post('/players/:id/comments', commentsCtrl.create);
 
 module.exports = router;
